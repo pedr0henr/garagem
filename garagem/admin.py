3957b47cf51fabc8b5e9af7b31c7aca4806be9bd
Gustavo Rudolf <gustavorudolfifc@gmail.com>
2023-03-17 11:04:57 -0300

    correção do commit class categoria

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
-from garagem.models import Marca
-from garagem.models import Categoria
+from garagem.models import Marca, Categoria
 
 admin.site.register(Marca)
 admin.site.register(Categoria)
